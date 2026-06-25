02577b07734c3b2be208b2af0d1d9101a79d8e1c
mrafee71 <muhammadrafeequeph@gmail.com>
2026-06-25 12:06:31 +0530

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Hero() {
+    return (
+        <div className="min-h-screen flex flex-col justify-center">
+            <h1 className="text-3xl italic text-white md:text-4xl lg:text-5xl font-bold mb-4 text-center">Plot Pixel</h1>
+            <div className="mx-auto px-4 md:px-8 lg:px-24 xl:px-48">
+                <h1 className="text-3xl text-white md:text-4xl lg:text-5xl font-bold mb-4 text-center">Turning Moments Into Masterpieces</h1>
+                <p className="text-sm text-zinc-500 md:text-md lg:text-lg italic text-center">Professional event photography and cinematic videography for weddings, engagements, birthdays, and special occasions. Preserving your memories, one pixel at a time.</p>
+            </div>
+        </div>
+    );
+}
